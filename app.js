4e830ba3d8fa968cb3d31bac2344496689391874
Tanner Jackley <tannerjackley@gmail.com>
2025-03-31 22:27:12 -0500

    pulling in images

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var yachtsRouter = require('./routes/yachts');
 var gridRouter = require('./routes/grid');
+var randomitemRouter = require('./routes/randomitem');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/yachts', yachtsRouter);
 app.use('/grid', gridRouter);
+app.use('/randomitem', randomitemRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
